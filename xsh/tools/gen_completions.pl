a17a19dacb4d214bf690b245f572c0dad444c6d2
Petr Pajas <pajas@matfyz.cz>
2002-03-06 08:31:04 +0000

    Initial commit.
    git-svn-id: https://svn.code.sf.net/p/xsh/svn/trunk@57 a80f4d36-b344-0410-8b47-b32d68e76229

---
+++ xsh/tools/gen_completions.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl
+
+# $Id: gen_completions.pl,v 1.1 2002-03-06 08:31:04 pajas Exp $
+
+use strict;
+use XML::LibXML;
+use Text::Wrap qw(wrap);
+
+if ($ARGV[0]=~'^(-h|--help)?$') {
+  print <<EOF;
+Generates a command-list module from RecDescentXML source.
+
+Usage: $0 <source.xml>
+
+EOF
+  exit;
+}
+
+print <<'EOF';
+package XSH::CompletionList;
+
+use strict;
+use vars qw(@XSH_COMMANDS);
+
+@XSH_COMMANDS=qw(
+EOF
+
+sub get_name {
+  my ($r)=@_;
+  return $r->getAttribute('name') ne ""
+    ? $r->getAttribute('name')
+      : $r->getAttribute('id');
+}
+
+my $parser=XML::LibXML->new();
+my $doc=$parser->parse_file($ARGV[0]);
+
+my $dom=$doc->getDocumentElement();
+
+foreach (sort map { get_name($_) }
+	 $dom->findnodes('./rules/rule[@type="command"]'),
+	 $dom->findnodes('./rules/rule[@type="command"]/aliases/alias')) {
+  print "$_\n";
+}
+
+print ");\n\n1;\n";
