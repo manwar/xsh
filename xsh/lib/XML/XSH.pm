68365064afb2310b340d0fb1bd44c31edbf63648
Petr Pajas <pajas@matfyz.cz>
2002-08-30 17:08:37 +0000

    XSH namespace now followed by a slash
    git-svn-id: https://svn.code.sf.net/p/xsh/svn/trunk@138 a80f4d36-b344-0410-8b47-b32d68e76229

--- xsh/lib/XML/XSH.pm
+++ xsh/lib/XML/XSH.pm
@@ -1,4 +1,4 @@
-# $Id: XSH.pm,v 1.4 2002-08-26 14:36:02 pajas Exp $
+# $Id: XSH.pm,v 1.5 2002-08-30 17:08:37 pajas Exp $
 
 package XML::XSH;
 
@@ -13,7 +13,7 @@ BEGIN {
   $VERSION   = '1.1';
   @ISA       = qw(Exporter);
   @EXPORT_OK = @XML::XSH::Functions::EXPORT_OK;
-  $xshNS = 'http://xsh.sourceforge.net/xsh';
+  $xshNS = 'http://xsh.sourceforge.net/xsh/';
 }
 
 1;
