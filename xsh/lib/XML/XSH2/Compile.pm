11ba6ac162669644a1ae9c4e8fcb8358de2bd562
E. Choroba <choroba@matfyz.cz>
2013-09-24 22:58:34 +0000

    version update
    git-svn-id: https://svn.code.sf.net/p/xsh/svn/trunk@651 a80f4d36-b344-0410-8b47-b32d68e76229

--- xsh/lib/XML/XSH2/Compile.pm
+++ xsh/lib/XML/XSH2/Compile.pm
@@ -6,7 +6,7 @@ use Module::Compile -base;
 use vars qw($VERSION);
 
 use strict;
-  $VERSION='2.1.8'; # VERSION TEMPLATE
+  $VERSION='2.1.9'; # VERSION TEMPLATE
 
 sub pmc_compile {
   my ($class, $source) = @_;
