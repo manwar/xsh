d8c9110ab9be85674082400e0d1390dfe089b98d
E. Choroba <choroba@matfyz.cz>
2015-10-15 22:48:42 +0000

    change bump
    git-svn-id: https://svn.code.sf.net/p/xsh/svn/trunk@677 a80f4d36-b344-0410-8b47-b32d68e76229

--- xsh/lib/XML/XSH2/DummyXPathContext.pm
+++ xsh/lib/XML/XSH2/DummyXPathContext.pm
@@ -1,6 +1,6 @@
 package XML::XSH2::DummyXPathContext;
 
-  $VERSION='2.1.21'; # VERSION TEMPLATE
+  $VERSION='2.1.22'; # VERSION TEMPLATE
 
 sub new {
   my ($class,$node)=@_;
--- xsh/lib/XML/XSH2/LibXMLCompat.pm
+++ xsh/lib/XML/XSH2/LibXMLCompat.pm
@@ -9,7 +9,7 @@ use XML::LibXML::Iterator;
 use XML::LibXML::NodeList;
 use vars qw($VERSION);
 
-  $VERSION='2.1.21'; # VERSION TEMPLATE
+  $VERSION='2.1.22'; # VERSION TEMPLATE
 
 sub module {
   return "XML::LibXML";
